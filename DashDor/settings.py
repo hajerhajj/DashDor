b0aeea1f3ca7c1cdbf31707c7680d9b3bf651645
hajerhajj <128532009+hajerhajj@users.noreply.github.com>
2024-05-16 19:25:31 +0100

    Update settings.py

--- DashDor/settings.py
+++ DashDor/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-90r2x_se7oqu^vjx6x^#w992px#-4(4d2vn!8x++e1+yj4nhg7'
 
-
-
-env=environ.Env()
-environ.Env.read_env()
-SECRET_KEY= env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
